a5061be761e552564c6d2c111c6bf4057b88c659
Timo Zehnle <timo.zehnle@reservix.de>
2021-07-15 15:04:47 +0200

    update button

--- packages/components/src/Button/Button.tsx
+++ packages/components/src/Button/Button.tsx
@@ -1,30 +1,47 @@
-import React, { useRef } from 'react';
+import React, { forwardRef, RefObject } from 'react';
 import { useButton } from 'react-aria';
-import { ComponentPropsWithRef } from '@marigold/types';
+import {
+  PolymorphicComponentWithRef,
+  PolymorphicPropsWithRef,
+} from '@marigold/types';
 
 import { Box, BoxOwnProps } from '../Box';
 
-export type ButtonProps = {
-  variant?: string;
-} & BoxOwnProps &
-  ComponentPropsWithRef<'button'>;
+export type ButtonProps = PolymorphicPropsWithRef<BoxOwnProps, 'button'>;
 
-export const Button = ({ variant = 'primary.large', ...props }) => {
-  const ref = useRef<any>();
-  const { buttonProps } = useButton(props, ref);
-  const { children } = props;
+export const Button: PolymorphicComponentWithRef<BoxOwnProps, 'button'> =
+  forwardRef(
+    (
+      {
+        as = 'button',
+        variant = 'primary.large',
+        children,
+        className,
+        ...props
+      },
+      ref
+    ) => {
+      const { buttonProps } = useButton(
+        {
+          ...props,
+          elementType: typeof as === 'string' ? as : 'span',
+        },
+        ref as RefObject<HTMLSpanElement>
+      );
 
-  return (
-    <Box
-      {...props}
-      {...buttonProps}
-      as="button"
-      variant={`button.${variant}`}
-      ref={ref}
-    >
-      <Box as="span" display="inline-flex" alignItems="center">
-        {children}
-      </Box>
-    </Box>
+      return (
+        <Box
+          {...props}
+          {...buttonProps}
+          as={as}
+          variant={`button.${variant}`}
+          className={className}
+          ref={ref}
+        >
+          <Box as="span" display="inline-flex" alignItems="center">
+            {children}
+          </Box>
+        </Box>
+      );
+    }
   );
-};
